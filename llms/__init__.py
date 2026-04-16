cf9d7390fd0a8cc44838adaf7e966a5aea0a4d83
MiguAlejandro <alejandro@fenixoft.ai>
2026-04-16 09:10:53 -0500

    Delete llms directory

--- llms/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import BaseLLM, create_llm
-from .openai_llm import OpenAILLM
-from .anthropic_llm import AnthropicLLM
-from .runpod_llm import RunpodLLM
-
-__all__ = ["BaseLLM", "create_llm", "OpenAILLM", "AnthropicLLM", "RunpodLLM"]
